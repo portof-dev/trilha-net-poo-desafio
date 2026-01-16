46737ebbe5820cafcf0a2ac1775ce074aeafd1bd
Felipe Porto <felipeporto423@gmail.com>
2026-01-16 16:52:49 -0300

    Projeto concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone novoIphone = new Iphone("12","Plus", "567123",28);
+novoIphone.Ligar();
+novoIphone.ReceberLigacao();
+novoIphone.InstalarAplicativo("Facebook");
+
+
+
+Nokia novoNokia = new Nokia ("12","Edge","1234",12);
+novoNokia.Ligar();
+novoNokia.ReceberLigacao();
+novoNokia.InstalarAplicativo("Whatsapp");
+
+
+
+
